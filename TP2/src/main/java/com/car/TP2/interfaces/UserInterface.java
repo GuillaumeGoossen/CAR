bbf18d31dfb5d3c9049ac0a91110901e1a32bb3f
Guillaume Goossen <guillaume.goossen.etu@univ-lille.fr>
2025-02-06 11:29:52 +0100

    Mise en place du service, repository et interface

---
+++ TP2/src/main/java/com/car/TP2/interfaces/UserInterface.java
@@ -0,0 +1,10 @@
+package com.car.TP2.interfaces;
+
+/*
+ * This interface represents a user interface
+ */
+public interface UserInterface {
+
+    public void createUser(String email, String password, String firstName, String lastName);
+
+}
---
+++ TP2/src/main/java/com/car/TP2/service/UserService.java
@@ -0,0 +1,28 @@
+package com.car.TP2.service;
+
+import com.car.TP2.entity.User;
+import com.car.TP2.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/*
+ * This class represents a user service
+ */
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public User login(String email, String password) {
+        return userRepository.findByEmailAndPassword(email, password);
+    }
+
+    public void save(User user) {
+        userRepository.save(user);
+    }
+
+    public boolean existsByEmail(String email) {
+        return userRepository.existsByEmail(email);
+    }
+}
